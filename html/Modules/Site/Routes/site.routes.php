52017afd5bcd9af43c66a13be6235a297389f8b4
ScorpioCoding <dev@scorpiocoding.com>
2024-12-21 05:40:38 +0100

    Refactoring

---
+++ html/Modules/Site/Routes/site.routes.php
@@ -0,0 +1,76 @@
+<?php
+
+return (object) array(
+
+
+  '' => [
+    'lang' => 'en',
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Home',
+    'action' => 'index'
+  ],
+
+  '/' => [
+    'lang' => 'en',
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Home',
+    'action' => 'index'
+  ],
+
+  '/{lang}/home' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Home',
+    'action' => 'index'
+  ],
+
+  '/{lang}/about' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'About',
+    'action' => 'index'
+  ],
+
+  '/{lang}/gallery' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Gallery',
+    'action' => 'index'
+  ],
+
+  '/{lang}/activity' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Activity',
+    'action' => 'index'
+  ],
+
+  '/{lang}/booking' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Booking',
+    'action' => 'index'
+  ],
+
+  '/{lang}/contact' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Contact',
+    'action' => 'index'
+  ],
+
+  '/{lang}/mail' => [
+    'module' => 'Site',
+    'namespace' => 'Modules\Site\Controllers',
+    'controller' => 'Mail',
+    'action' => 'index'
+  ]
+
+
+
+
+
+
+);
